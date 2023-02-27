fa59225f09b9c5d10c9d43beb3f06aaf558e6278
andres98100 <andressolarte580@gmail.com>
2023-02-27 14:39:36 -0500

    Readme

---
+++ models/base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''import'''
+import uuid
+import time
+from time import sleep
+from datetime import datetime
+'''class'''
+
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
