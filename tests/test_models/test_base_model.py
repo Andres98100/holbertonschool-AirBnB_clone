88cd9bfbdd3c8ab7005b3d487faa4a5690224acb
andres98100 <andressolarte580@gmail.com>
2023-02-27 18:25:08 -0500

    save and update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,11 +7,17 @@ from datetime import datetime
 
 
 class Test_base_model(unittest.TestCase):
-    """method test save"""
+    """method test created"""
     def test_creation(self):
         """test"""
         obj = BaseModel()
         self.assertEqual(datetime, type(obj.updated_at))
-    """method """
+    """method to_dict"""
+    def test_save(self):
+        """test"""
+        obj = BaseModel()
+        obj.save()
+        self.assertIsInstance(obj.updated_at, datetime)
+
 if __name__ == "__main__":
     unittest.main()
