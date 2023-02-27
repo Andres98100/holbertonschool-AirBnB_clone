fde40b2f066e9d79c0451de3fb2a52fa444e3725
andres98100 <andressolarte580@gmail.com>
2023-02-27 17:07:06 -0500

    update airbnb

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
-# holbertonschool-AirBnB_clone :snake:
+<h1 align="center">
+    <img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Airbnb_icon-icons.com_66791.png" height="30" alt="AirBnB logo"> AirBnB clone
+</h1>
 
-<p align="center" width="70%">
-    <img src="img/hbnb.png">
+<p align="center">
+    <img src="img/hbnb.png" alt="holberton logo">
+</p>
+
+<h2>
+     Description :speech_balloon:
+</h2>
+
+<p>
+    in this project a console of the Airbnb web application is recreated, the tools used for the completion of the project were: <br>
+    <img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Airbnb_icon-icons.com_66791.png" height="10" alt="AirBnB logo"> python3 <br>
+    <img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Airbnb_icon-icons.com_66791.png" height="10" alt="AirBnB logo"> json files where a database is  simulated and also serves to translate  data between languages <br>
+    <img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Airbnb_icon-icons.com_66791.png" height="10" alt="AirBnB logo"> PyCodeStyle
 </p>
 
--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-'''import'''
+"""import"""
 import uuid
-import time
-from time import sleep
 from datetime import datetime
-'''class'''
+"""class"""
 
 
 class BaseModel:
+    """method init"""
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key in ["created_at", "update_at"]:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
 
     def __str__(self):
+        """return"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
+        """update the date"""
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
+        """new dictionary"""
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
+        """return new dictionary"""
         return new_dict
