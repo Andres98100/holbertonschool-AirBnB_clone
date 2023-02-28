7f621a9ac05cd482486888b121c9966f94b8b80b
andres98100 <andressolarte580@gmail.com>
2023-02-27 19:47:16 -0500

    kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -10,15 +10,16 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now()    
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
-                elif key in ["created_at", "update_at"]:
+                elif key in ["created_at", "updated_at"]:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
-
+                elif key == "id":
+                    self.id = value
     def __str__(self):
         """return"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
