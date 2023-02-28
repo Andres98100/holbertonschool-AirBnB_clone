924dcec2db0fd5b869d96bb9c16123a459e6750a
andres98100 <andressolarte580@gmail.com>
2023-02-27 19:33:30 -0500

    dict

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,6 +16,11 @@ class Test_base_model(unittest.TestCase):
     def setUp(self):
         """assign the class"""
         self.obj = BaseModel()
+        self.obj.id = "test_id"
+        self.obj.created_at = datetime.now()
+        self.obj.updated_at = datetime.now()
+        self.obj.name = "Test Name"
+        self.obj.age = 30
     """method save"""
     def test_save(self):
         """Test that save updates the updated_at attribute"""
@@ -31,9 +36,12 @@ class Test_base_model(unittest.TestCase):
     """method to_dict"""
     def test_dict(self):
         """test dict"""
-        original = self.obj.to_dict()
-        new = self.obj.to_dict()
-        self.assertEquals(original, new)
+        original = {"id": self.obj.id,
+                    "__class__": self.obj.__class__.__name__,
+                    "created_at": self.obj.created_at,
+                    "updated_at": self.obj.updated_at,
+                    }
+        self.assertNotEqual(original, self.obj.to_dict())
 
 if __name__ == "__main__":
     unittest.main()
