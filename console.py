3e152bf28cafdd34135000776691fdc570ea40b1
CharlineMosquera <char951144@gmail.com>
2023-03-01 17:10:02 -0500

    documentation

--- console.py
+++ console.py
@@ -8,9 +8,11 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, line):
+        """to exit the program"""
         return True
     
     def do_EOF(self, line):
+        """to exit the program"""
         return True
 
     def emptyline(self):
