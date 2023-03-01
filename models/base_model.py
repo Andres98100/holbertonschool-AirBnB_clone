84c7104e3bafe58b4eade1081afe291270b37888
andres98100 <andressolarte580@gmail.com>
2023-03-01 08:09:04 -0500

    update classes

--- models/base_model.py
+++ models/base_model.py
@@ -15,9 +15,12 @@ class BaseModel:
                 if key == "__class__":
                     pass
                 else:
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.strptime(value,
+                                                            "%Y-%m-%dT%H:%M:%S.%f")
+                        value = datetime.strptime(value,
+                                                            "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
-            self.created_at = datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
-            self.updated_at = datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
