bdb826e2a11fd418902670a1fff3b930887d3bee
CharlineMosquera <char951144@gmail.com>
2023-03-01 17:16:28 -0500

    style console

--- console.py
+++ console.py
@@ -6,13 +6,13 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """A custom command line class for the AirBnB
     project based on the base class cmd"""
-    
+
     prompt = "(hbnb) "
 
     def do_quit(self, line):
         """to exit the program"""
         return True
-    
+
     def do_EOF(self, line):
         """to exit the program"""
         return True
@@ -20,6 +20,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """empty line + ENTER is not executed"""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
