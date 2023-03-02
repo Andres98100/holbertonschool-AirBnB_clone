30b269e25b500842b3d8d5f608980975a21b2242
andres98100 <andressolarte580@gmail.com>
2023-03-02 17:29:51 -0500

    solution user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,10 +8,18 @@ from models.user import User
 
 class TestUser(unittest.TestCase):
     """testing the correct functioning of the class"""
-    def test_email(self):
-        user_email = User().email
-        user_password = User().password
-    def test_email(self):
-        User().email
+    def test_str_attr(self):
+        user = User()
         self.assertEqual(User().email, "")
-    pass
+        self.assertEqual(User().password, "")
+        self.assertEqual(User().first_name, "")
+        self.assertEqual(User().last_name, "")
+    def test_str(self):
+        user = User()
+        self.assertEqual(type(user.email), str)
+        self.assertEqual(type(user.password), str)
+        self.assertEqual(type(user.first_name), str)
+        self.assertEqual(type(user.last_name), str)
+
+if __name__ == "__main__":
+    unittest.main()
