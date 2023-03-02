3dba7e86dd88d35144a73e647af573e089b30ac7
andres98100 <andressolarte580@gmail.com>
2023-03-02 17:23:31 -0500

    user email test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,4 +8,10 @@ from models.user import User
 
 class TestUser(unittest.TestCase):
     """testing the correct functioning of the class"""
+    def test_email(self):
+        user_email = User().email
+        user_password = User().password
+    def test_email(self):
+        User().email
+        self.assertEqual(User().email, "")
     pass
